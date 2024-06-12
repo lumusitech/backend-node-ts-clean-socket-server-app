74c11ef9f3322f78089a0f5c57b19c88f7945dca
Luciano Figueroa <lumusitech@gmail.com>
2024-06-12 14:03:43 -0300

    feat: add wss service

---
+++ src/presentation/services/wss.service.ts
@@ -0,0 +1,47 @@
+import type { Server } from 'http';
+import { WebSocketServer, type WebSocket } from 'ws';
+
+interface Options {
+  server: Server
+  path?: string
+}
+
+// Singleton because we only need one instance of this service
+export class WssService {
+
+  private static _instance: WssService;
+  private wss: WebSocketServer;
+
+  private constructor(options: Options) {
+    const { server, path = '/ws' } = options; // default path: ws://localhost:3000/ws
+
+    this.wss = new WebSocketServer({ server, path });
+
+    this.start();
+  }
+
+  static get instance(): WssService {
+    if (!WssService._instance) {
+      throw 'WssService not initialized';
+    }
+    return WssService._instance;
+  }
+
+  static initWss(options: Options) {
+    if (WssService._instance) {
+      throw 'WssService already initialized';
+    }
+    WssService._instance = new WssService(options);
+  }
+
+  public start() {
+    this.wss.on("connection", (ws: WebSocket) => {
+      console.log("Client connected");
+
+      ws.on("close", () => {
+        console.log("Client disconnected");
+      })
+
+    })
+  }
+}
