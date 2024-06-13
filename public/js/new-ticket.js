7d4b099ecc6d52ca7330a2168806b80f3b8886f3
Luciano Figueroa <lumusitech@gmail.com>
2024-06-12 22:24:28 -0300

    feat: implement script for new-ticket view

--- public/js/new-ticket.js
+++ public/js/new-ticket.js
@@ -1,3 +1,15 @@
+const currentTicketLbl = document.querySelector('#lbl-new-ticket')
+const createTicketBtn = document.querySelector('button')
 
+const getLastTicket = async () => {
+  const lastTicket = await fetch('/api/ticket/last').then(resp => resp.json())
+  currentTicketLbl.innerText = lastTicket
+}
 
-console.log('Nuevo Ticket HTML');
+const createTicket = async () => {
+  const ticket = await fetch('/api/ticket', { method: 'POST' }).then(resp => resp.json())
+  currentTicketLbl.innerText = ticket.number
+}
+
+createTicketBtn.addEventListener('click', createTicket)
+getLastTicket()
